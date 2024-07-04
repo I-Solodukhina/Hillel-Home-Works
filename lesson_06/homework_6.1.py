d51f842b4bffd88d7be1182320a4843ac301a3fc
Iryna_S <i.solodukhina.w@gmail.com>
2024-07-04 22:32:54 +0300

    ДЗ 6.1 та 6.4

---
+++ lesson_06/homework_6.1.py
@@ -0,0 +1,8 @@
+text_we_need_for_the_hw = set(
+    input('Complete the sentence: "If you\'re happy and you know it " - '))  # clap your hand :)
+print(text_we_need_for_the_hw)
+
+if len(text_we_need_for_the_hw) > 10:
+    print(True)
+else:
+    print(False)
